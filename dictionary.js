9e17c30b1b4f312260882a97c62ac9ef73a6453e
thuy-do-203 <dothuy20397@gmail.com>
2022-05-28 00:16:40 +0700

    Thùy push 28.05.22

--- dictionary.js
+++ dictionary.js
@@ -1,14 +1,54 @@
 // Set random bgr change after 5min
 window.onload = function () {
-    var backgroundImg=["/Asset/Background/ambrose-chua-zxbNbuncq1g-unsplash.jpg",
-                        "/Asset/Background/Back ground image 1.png",
-                        "/Asset/Background/bgr2.jpg",
-                        "/Asset/Background/bgr33.jpg"
-                        ]
+    var backgroundImg=["Asset/Background/Dic bgr/dic bgr 1.jpg",
+    "Asset/Background/Dic bgr/dic bgr 2.jpg",
+    "Asset/Background/Dic bgr/dic bgr 3.jpg",
+    "Asset/Background/Dic bgr/dic bgr 4.jpg",
+    "Asset/Background/Dic bgr/dic bgr 5.jpg",
+    "Asset/Background/Dic bgr/dic bgr 6.jpg",
+    "Asset/Background/Dic bgr/dic bgr 7.jpg",
+    "Asset/Background/Dic bgr/dic bgr 8.jpg",
+    "Asset/Background/Dic bgr/dic bgr 9.jpg",
+    "Asset/Background/Dic bgr/dic bgr 10.jpg",
+    "Asset/Background/Dic bgr/dic bgr 11.jpg",
+    "Asset/Background/Dic bgr/dic bgr 12.jpg",
+    "Asset/Background/Dic bgr/dic bgr 13.jpg",
+    "Asset/Background/Dic bgr/dic bgr 14.jpg",
+    "Asset/Background/Dic bgr/dic bgr 15.jpg",
+    "Asset/Background/Dic bgr/dic bgr 16.jpg",
+    "Asset/Background/Dic bgr/dic bgr 17.jpg",
+    "Asset/Background/Dic bgr/dic bgr 18.jpg",
+    "Asset/Background/Dic bgr/dic bgr 19.jpg",
+    "Asset/Background/Dic bgr/dic bgr 20.jpg",
+    "Asset/Background/Dic bgr/dic bgr 21.jpg",
+    "Asset/Background/Dic bgr/dic bgr 22.jpg",
+    "Asset/Background/Dic bgr/dic bgr 23.jpg",
+    "Asset/Background/Dic bgr/dic bgr 24.jpg",
+    "Asset/Background/Dic bgr/dic bgr 25.jpg",
+    "Asset/Background/Dic bgr/dic bgr 26.jpg",
+    "Asset/Background/Dic bgr/dic bgr 27.jpg",
+    "Asset/Background/Dic bgr/dic bgr 28.jpg",
+    "Asset/Background/Dic bgr/dic bgr 29.jpg",
+    "Asset/Background/Dic bgr/dic bgr 30.jpg",
+    "Asset/Background/Dic bgr/dic bgr 31.jpg",
+    "Asset/Background/Dic bgr/dic bgr 32.jpg",
+    "Asset/Background/Dic bgr/dic bgr 33.jpg",
+    "Asset/Background/Dic bgr/dic bgr 34.jpg",
+    "Asset/Background/Dic bgr/dic bgr 35.jpg",
+    "Asset/Background/Dic bgr/dic bgr 36.jpg",
+    "Asset/Background/Dic bgr/dic bgr 37.jpg",
+    "Asset/Background/Dic bgr/dic bgr 38.jpg",
+    "Asset/Background/Dic bgr/dic bgr 39.jpg",
+    "Asset/Background/Dic bgr/dic bgr 40.jpg",
+    "Asset/Background/Dic bgr/dic bgr 41.jpg",
+    "Asset/Background/Dic bgr/dic bgr 42.jpg",
+    "Asset/Background/Dic bgr/dic bgr 43.jpg",
+    "Asset/Background/Dic bgr/dic bgr 44.jpg"
+    ]
 
-    setInterval(changeImage, 50000);
+    setInterval(changeImage, 60000);
     function changeImage() {   
-        var i = Math.floor((Math.random() * 4));
+        var i = Math.floor((Math.random() * 43));
         document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
     }
    }
