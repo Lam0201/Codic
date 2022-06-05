37bb482e5157e08c97007973f4ccaf5b6d15578d
thuy-do-203 <dothuy20397@gmail.com>
2022-06-06 06:05:43 +0700

    Hoàn thiện database

--- dictionary.js
+++ dictionary.js
@@ -76,16 +76,20 @@ function data(result, word) {
     }
 }
 
+
 function search(word) {
     fetchApi(word);
     searchInput.value = word;
 }
 
 function fetchApi(word) {
+    // Xóa class active của wrapper
     wrapper.classList.remove("active");
     infoText.style.color = "#000";
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}  `;
+
+    // 
     fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() => {
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
     });
